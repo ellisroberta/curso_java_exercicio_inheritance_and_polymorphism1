183bfee12692429ada870da89df009dfa45cf372
ellisroberta <ellisroberta@hotmail.com>
2024-10-30 17:18:48 -0300

    organizacao das pastas, edicao do .gitignore e realizacao do exercicio

--- application/src/application/Program.java
+++ application/src/application/Program.java
@@ -21,35 +21,35 @@ public class Program {
 
         List<Product> list = new ArrayList<>();
 
-        System.out.print("Entre com o numero de produtos: ");
+        System.out.print("Enter the number of products: ");
         int n = sc.nextInt();
 
         for (int i=1; i<=n; i++) {
-            System.out.println("Datos do produto #" + i + ":");
-            System.out.print("Comum, usado, importado (c/u/i)? ");
+            System.out.println("Product #" + i + " data:");
+            System.out.print("Common, used or imported (c/u/i)? ");
             char type = sc.next().charAt(0);
-            System.out.print("Nome: ");
+            System.out.print("Name: ");
             sc.nextLine();
             String name = sc.nextLine();
-            System.out.print("Preco: ");
+            System.out.print("Price: ");
             double price = sc.nextDouble();
             if (type == 'c') {
                 list.add(new Product(name, price));
             }
             else if (type == 'u') {
-                System.out.print("Data de fabricacao (DD/MM/YYYY): ");
+                System.out.print("Manufacture date (DD/MM/YYYY): ");
                 LocalDate date = LocalDate.parse(sc.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
                 list.add(new UsedProduct(name, price, date));
             }
             else {
-                System.out.print("Taxa de importacao: ");
+                System.out.print("Customs fee: ");
                 double customsFee = sc.nextDouble();
                 list.add(new ImportedProduct(name, price, customsFee));
             }
         }
 
         System.out.println();
-        System.out.println("ETIQUETAS DE PRECO:");
+        System.out.println("PRICE TAGS:");
         for (Product prod : list) {
             System.out.println(prod.priceTag());
         }
